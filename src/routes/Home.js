32dda1590a34f06910d500bac6efb5d87dfecc93
Youssef Selkani <Ynakles@gmail.com>
2018-10-07 08:15:39 +0100

    hard coded cards

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 class Home extends Component {
 	render() {
 		return (
-			<Container>
-				<h1 className="text-light pt-5 pb-5">Home</h1>
-			</Container>
+			<div class="row">
+				<div class="col-md-4 mb-3">
+					<Card>
+						<CardImg
+							top
+							width="100%"
+							src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+							alt="Card image cap"
+						/>
+						<CardBody>
+							<CardTitle>Card title</CardTitle>
+							<CardSubtitle>Card subtitle</CardSubtitle>
+							<CardText>
+								Some quick example text to build on the card title and make up the bulk of the card's content.
+							</CardText>
+							<Button>Button</Button>
+						</CardBody>
+					</Card>
+				</div>
+				<div class="col-md-4 mb-3">
+					<Card>
+						<CardImg
+							top
+							width="100%"
+							src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+							alt="Card image cap"
+						/>
+						<CardBody>
+							<CardTitle>Card title</CardTitle>
+							<CardSubtitle>Card subtitle</CardSubtitle>
+							<CardText>
+								Some quick example text to build on the card title and make up the bulk of the card's content.
+							</CardText>
+							<Button>Button</Button>
+						</CardBody>
+					</Card>
+				</div>
+				<div class="col-md-4 mb-3">
+					<Card>
+						<CardImg
+							top
+							width="100%"
+							src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+							alt="Card image cap"
+						/>
+						<CardBody>
+							<CardTitle>Card title</CardTitle>
+							<CardSubtitle>Card subtitle</CardSubtitle>
+							<CardText>
+								Some quick example text to build on the card title and make up the bulk of the card's content.
+							</CardText>
+							<Button>Button</Button>
+						</CardBody>
+					</Card>
+				</div>
+			</div>
 		);
 	}
 }
