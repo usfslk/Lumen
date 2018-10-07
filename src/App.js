38c0a1c767ffea3cee2f62b38f6dfe697a3e14c5
Youssef Selkani <Ynakles@gmail.com>
2018-10-07 07:29:39 +0100

    login form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Button } from "reactstrap";
+import { Container } from "reactstrap";
+
+class Form extends Component {
+	render() {
+		return (
+			<div class="form">
+				<form class="login-form">
+					<input
+						type="text"
+						onChange={this.handleChange}
+						placeholder="Email"
+						name="email"
+						autoComplete="username"
+					/>
+					<input
+						type="password"
+						onChange={this.handleChange}
+						placeholder="Password"
+						name="password"
+						autoComplete="current-password"
+					/>
+					<button>login</button>
+					<div class="m-3" />
+					<button>signup</button>
+					<p class="message">
+						Your password must not contain spaces, special characters, or
+						emojis. By continuing you agree to our{" "}
+						<a href="#">terms of services</a>
+					</p>
+				</form>
+			</div>
+		);
+	}
+}
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<p>
-						Hello <code>Lumen.js</code>
-						<Button color="danger">Danger!</Button>
-					</p>
-				</header>
-			</div>
+			<Container>
+				<h1 className="text-center text-light pt-5 pb-5">Lumen</h1>
+				<Form />
+			</Container>
 		);
 	}
 }
