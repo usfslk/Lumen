6c720f4084de77cf0569080400371e9e13c859dd
Youssef Selkani <Ynakles@gmail.com>
2018-10-07 07:39:10 +0100

    login / signup functions

--- src/App.js
+++ src/App.js
@@ -15,24 +15,40 @@ class Form extends Component {
 	handleChange = e => {
 		this.setState({ [e.target.name]: e.target.value });
 	};
+	login(e) {
+		this.setState({ loading: true });
+		e.preventDefault();
+		fire
+			.auth()
+			.signInWithEmailAndPassword(this.state.email, this.state.password)
+			.then(u => {
+				this.setState({ loading: false });
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
+
+	signup(e) {
+		this.setState({ loading: true });
+		e.preventDefault();
+		fire
+			.auth()
+			.createUserWithEmailAndPassword(this.state.email, this.state.password)
+			.then(u => {})
+			.then(u => {
+				this.setState({ loading: false });
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 	render() {
 		return (
 			<div class="form">
 				<form class="login-form">
-					<input
-						type="text"
-						onChange={this.handleChange}
-						placeholder="Email"
-						name="email"
-						autoComplete="username"
-					/>
-					<input
-						type="password"
-						onChange={this.handleChange}
-						placeholder="Password"
-						name="password"
-						autoComplete="current-password"
-					/>
+					<input type="text" onChange={this.handleChange} placeholder="Email" name="email" autoComplete="username" />
+					<input type="password" onChange={this.handleChange} placeholder="Password" name="password" autoComplete="current-password" />
 					<button onClick={this.login}>login</button>
 					<div class="m-3" />
 					<button onClick={this.signup}>signup</button>
