04056cf47768d9b06418e57a5eb02b8441e83a2c
Youssef Selkani <Ynakles@gmail.com>
2018-10-07 06:19:44 +0100

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Button } from "reactstrap";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Hello <code>Lumen.js</code>
-          </p>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>
+						Hello <code>Lumen.js</code>
+						<Button color="danger">Danger!</Button>
+					</p>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
